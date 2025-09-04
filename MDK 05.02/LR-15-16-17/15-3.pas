d42979b4da4b11aef5289dd499144ef89b771086
YSLnerd <vovalapt23@gmail.com>
2025-09-04 12:32:40 +0300

    Create 15-3.pas

---
+++ MDK 05.02/LR-15-16-17/15-3.pas
@@ -0,0 +1,25 @@
+﻿program qq;
+type toy=record
+  name:string;
+  price:string;
+  age:1..11;
+end;
+var
+  arr: array[1..3] of toy;
+  i:integer;
+  begin
+      with arr[1] do
+        begin
+        arr[1].name:='мяч';
+      arr[1].price:='300';
+      arr[1].age:=7;
+      end;
+      with arr[2] do
+        begin
+        arr[2].name:='мeч';
+      arr[2].price:='400';
+      arr[2].age:=8;
+      end;
+      writeln('Имя: ',arr[1].name,' Цена: ',arr[1].price,' Рекомендемый возраст: ',arr[1].age);
+     writeln('Имя: ',arr[2].name,' Цена: ',arr[2].price,' Рекомендемый возраст: ',arr[2].age); 
+  end.
