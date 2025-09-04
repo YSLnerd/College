5ba73f2520e7f4cae692fd210a848f7108925ee4
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:40 +0300

    Create 7.pas

---
+++ OAIP/LR-13/7.pas
@@ -0,0 +1,23 @@
+﻿var
+  filetext: file of char; 
+  ch: char;
+  num: integer;
+
+begin
+  assign(filetext, 'D:\\основы программирования и алгортмизации\\лаба 13\\voskl.txt');
+  reset(filetext); 
+  num := 0;         
+  while not eof(filetext) do
+  begin
+    read(filetext, ch);  
+    if (num mod 2 <> 0) then 
+    begin
+      ch := '!';         
+      seek(filetext, num); 
+      write(filetext, ch); 
+    end;
+    num += 1;
+  end;
+  close(filetext); 
+  writeln('Программа выполнена');
+end.
