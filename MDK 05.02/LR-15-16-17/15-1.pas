cd9ef4399cc6369b6361dc08fed26189421a5350
YSLnerd <vovalapt23@gmail.com>
2025-09-04 12:32:38 +0300

    Create 15-1.pas

---
+++ MDK 05.02/LR-15-16-17/15-1.pas
@@ -0,0 +1,12 @@
+﻿type anketa= record
+  fio:string;
+  birth:string;
+  kurs:1..5;
+end;
+var student:anketa;
+begin
+  student.fio:='John Doe';
+  student.birth:='25.11.2009';
+  student.kurs:=1;
+  writeln(student.fio,'/',student.birth,'/',student.kurs);
+end.
