11d1f12053f20876d1e5ce7013742cbe87b99166
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:08 +0300

    Create 2.pas

---
+++ OAIP/Lr-12/2.pas
@@ -0,0 +1,21 @@
+﻿program qq;
+var
+   filetext: text;
+   i,n,k,j:integer;
+begin
+  write('Введите количество строк ');
+  Readln(n);
+  write('Введите количество символов в строке ');
+  readln(k);
+  assign(filetext,'D:\основы программирования и алгортмизации\лаба 12\text2.txt');
+  rewrite(filetext);
+  for i := 1 to n do
+  begin
+    for j := 1 to k do
+      begin
+       write(filetext, '*'); 
+    end;
+    writeln(filetext);
+    end;
+    close(filetext);
+end.
