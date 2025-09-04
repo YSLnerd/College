cdc7934998b35b8221507a6b8edc0e6c869ae6d9
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:33 +0300

    Create 4.pas

---
+++ OAIP/LR-13/4.pas
@@ -0,0 +1,24 @@
+﻿program qq;
+var
+  textfile: file of real;
+  num, sum,number: real;
+  index,i,n: integer;
+begin
+  sum := 0;
+  writeln('Введите количество чисел');
+  readln(n);
+  assign(textfile, 'D:\основы программирования и алгортмизации\лаба 13\sum.txt');
+  rewrite(textfile);
+  for i:=1 to n do
+  begin
+    write('Введите число ', i, ': ');
+    readLn(number); 
+    write(textfile, number);
+    if i mod 2 = 0 then
+      begin
+      sum+=number;
+      end;
+  end;
+  close(textfile);
+  writeln('Сумма элементов с четными номерами: ', sum:0:2);
+end.
