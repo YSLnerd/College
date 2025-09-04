bb15f49e8e7f7d4cc1573607500a61a31636d574
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:58 +0300

    Create lr2Task2.pas

---
+++ OAIP/LR-11-12/lr2Task2.pas
@@ -0,0 +1,23 @@
+﻿program qq;
+Uses GraphABC;
+begin
+  setpencolor(clblack);
+  line(150,400,450,400);
+  line(300,100,225,400);
+  line(300,100,375,400);
+  line(150,400,50,150);
+  line(50,150,250,300);
+  line(450,400,550,150);
+  line(550,150,350,300);
+  circle(300,100,30);
+  circle(50,150,30);
+  circle(300,100,30);
+  circle(550,150,30);
+  floodfill(50,150,clblue);
+  floodfill(550,150,cllime);
+  floodfill(300,100,clred);
+  floodfill(300,200,clred);
+  floodfill(200,390,clblue);
+  floodfill(440,390,cllime);
+
+  end.
