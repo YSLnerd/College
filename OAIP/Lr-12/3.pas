1a5e14497f833c07623b5d2882987f76343dece9
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:10 +0300

    Create 3.pas

---
+++ OAIP/Lr-12/3.pas
@@ -0,0 +1,13 @@
+﻿program qq;
+var
+   filetext: text;
+   s: string;
+begin
+   write('Введите строку ');
+   readln(s);
+   assign(filetext, 'D:\основы программирования и алгортмизации\лаба 12\text.txt');
+   append(filetext);
+   writeln(filetext, S);
+   close(filetext);  
+   writeln('Строка добавлена');
+end.
