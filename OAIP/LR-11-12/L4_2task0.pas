4491d76af19395f3850fe14b6c4abd7086bee6c2
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:47 +0300

    Create L4_2task0.pas

---
+++ OAIP/LR-11-12/L4_2task0.pas
@@ -0,0 +1,14 @@
+﻿program qq;
+Uses GraphABC;
+begin
+  setpencolor(clred);
+  MoveTo(30,30);
+  LineTo(100,30);
+  LineTo(100,100);
+  lineTo(30,100);
+  LineTo(30,30);
+  moveto(200,200);
+  LineTo(260,290);
+  lineto(140,290);
+  LineTo(200,200);
+  end.
