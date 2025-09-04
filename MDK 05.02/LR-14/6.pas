3e9bbe50a3891c6435eb2c3ad2951bc422a85f65
YSLnerd <vovalapt23@gmail.com>
2025-09-04 12:31:56 +0300

    Create 6.pas

---
+++ MDK 05.02/LR-14/6.pas
@@ -0,0 +1,9 @@
+﻿procedure fib(i, n: integer);//параметризация
+begin
+    writeln(i + n, ' '); 
+    if n < 55 then// база n=55 
+        fib(n, i + n);//декомпозиция
+end;
+begin
+    fib(0, 1);
+end.
