4fb22778909aed5f9e6f4e74fe9126dc3ee91700
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:31 +0300

    Create 3.pas

---
+++ OAIP/LR-13/3.pas
@@ -0,0 +1,55 @@
+﻿program qq;
+var
+  vesFile: file of real;
+  nechFile: file of real;
+  chFile: file of real;
+  number: real;
+  index, n: integer;
+begin
+  assign(vesFile, 'D:\\основы программирования и алгортмизации\\лаба 13\\vesch.txt');    
+  assign(nechFile, 'D:\\основы программирования и алгортмизации\\лаба 13\\nechyot.txt');        
+  assign(chFile, 'D:\\основы программирования и алгортмизации\\лаба 13\\chyot.txt');        
+  rewrite(vesFile);  
+  write('Введите количество чисел: ');
+  readLn(n);
+  for index := 1 to n do
+  begin
+    write('Введите число ', index, ': ');
+    readLn(number); 
+    write(vesFile, number); 
+  end;
+  close(vesFile); 
+  reset(vesFile);
+  rewrite(nechFile);
+  rewrite(chFile);
+  index := 1;
+  while not eof(vesFile) do
+  begin
+    read(vesFile, number); 
+    if index mod 2 = 1 then
+      write(nechFile, number)  
+    else
+      write(chFile, number); 
+    index := index + 1; 
+  end;
+  close(vesFile);
+  close(nechFile);
+  close(chFile);
+  writeln('Числа на нечётных позициях:');
+  reset(nechFile);
+  while not eof(nechFile) do
+  begin
+    read(nechFile, number);
+    write(number:0:2, ' ');
+  end;
+  close(nechFile);
+  writeln; 
+  writeln('Числа на чётных позициях:');
+  reset(chFile);
+  while not eof(chFile) do
+  begin
+    read(chFile, number);
+    write(number:0:2, ' ');
+  end;
+  close(chFile);
+end.
