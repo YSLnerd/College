683523a92de2794639e64e8ce77731d6cff1d79d
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:36 +0300

    Create Задание 2(2).pas

---
+++ OAIP/LR-1-2/Задание 2(2).pas
@@ -0,0 +1,24 @@
+﻿program задание_22;
+
+var
+  A,X,answer: real;
+  B: integer;
+
+begin
+  writeln('Введите стоимость каждой из первых 75 газет');
+  read(A);
+  writeln('Введите количество проданных газет');
+  read(B);
+  If (B<=75) then
+    begin
+    answer:=(A*B);
+    write(answer);
+    end;
+  If(B>75) then
+    begin
+    writeln('Введите стоимость каждой из последующих проданных газет');
+    read(X);
+    answer:=A*75+X*(B-75);
+    write(answer);
+    end;
+end.
