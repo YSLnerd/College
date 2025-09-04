e8e41cffe044e73175febb3c272556d16e7de785
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:50 +0300

    Create L4_2task1.pas

---
+++ OAIP/LR-11-12/L4_2task1.pas
@@ -0,0 +1,16 @@
+﻿program qq;
+Uses GraphABC;
+begin
+  setpenwidth(3);
+  line(100,200,500,200);
+  line(300,100,500,200);
+  line(300,100,100,200);
+  floodfill(300,150,clblue);
+  line(100,200,300,300);
+  line(300,300,500,200);
+  floodfill(300,250,cllime);
+  circle(50,200,50);
+  Circle(550,200,50);
+  floodfill(50,200,clred);
+  floodfill(550,200,clyellow);
+end.
