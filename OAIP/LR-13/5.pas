23bf617994bad47fed2820db1a48c500b6e1c49d
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:36 +0300

    Create 5.pas

---
+++ OAIP/LR-13/5.pas
@@ -0,0 +1,33 @@
+﻿
+program qq;
+var
+  i,ind,j,res:integer;
+  filetext: file of integer;
+  ARR: array[1..10] of integer;
+  begin
+  assign(filetext,'D:\основы программирования и алгортмизации\лаба 13\lokmax.txt');
+  rewrite(filetext);
+  write('Введите числа');
+  for i:=1 to 10 do 
+    begin
+    readln(arr[i]);
+    write(filetext, arr[i]);
+    end;
+  writeln('Числа записаны');
+  ind:=0;
+  for j:=2 to 9 do
+  begin
+    if (ARR[j]>ARR[j+1]) and (ARR[j]>ARR[j-1]) then
+      begin
+      ind:=j;
+      res:=ARR[j];
+      end;
+  end;
+  if ind= 0 then
+    writeln('Локальных максимумов нет')
+  else
+    begin
+  writeln('Последний локальный максимум: ', res);
+  end;
+  close(filetext);
+  end.
