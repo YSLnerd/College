fb551f65fc815ec45db18c1df14b5e4af0cd4249
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:52 +0300

    Create L4_2task3.pas

---
+++ OAIP/LR-11-12/L4_2task3.pas
@@ -0,0 +1,13 @@
+﻿program qq;
+Uses GraphABC;
+var 
+  i,x:integer;
+  begin
+    x:=50;
+    while x<=290 do
+      begin
+      SetPenColor(rgb(random(256), random(256), random(256))); 
+      circle(x,100,10);
+      x+=30;
+      end;
+  end.
