a48ac632d7241a3d14870a80dbf6281ac4c4e0e7
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:01:56 +0300

    Create L4_2task5.pas

---
+++ OAIP/LR-11-12/L4_2task5.pas
@@ -0,0 +1,16 @@
+﻿program qq;
+Uses GraphABC;
+var x, y ,r, i:integer;
+begin
+  x:=30;
+  y:=30;
+  r:=10;
+  for i:=1 to 8 do
+  begin
+    circle(x,y,r);
+    floodfill(x,y,rgb(random(256), random(256), random(256)));
+    x+=70;
+    y+=45;
+    r+=12;
+  end;
+end.
