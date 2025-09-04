655d47da1ea817c8735f76bf5aad86f5428bfa58
YSLnerd <vovalapt23@gmail.com>
2025-09-04 12:30:29 +0300

    Create 4.pas

---
+++ MDK 05.02/LR-14/4.pas
@@ -0,0 +1,11 @@
+﻿procedure LoopFor(i, n: integer);//параметризация
+begin
+  if i <= n then//декомпозиция
+  begin
+    writeln('привет ', i);
+    LoopFor(i + 1, n);
+  end;//база i>n
+end;
+begin
+  LoopFor(1, 10);
+end.
