f8709a7da08a320ccc27345d466735ab91ce70ac
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:26 +0300

    Create 1.pas

---
+++ OAIP/LR-13/1.pas
@@ -0,0 +1,29 @@
+﻿program qq;
+var
+  filetext: file of char;
+  x,i:integer;
+  s:string;
+  k:char;
+begin
+  assign(filetext,'D:\основы программирования и алгортмизации\лаба 13\text.txt');
+  reset(filetext);
+  while not eof(filetext) do
+  begin
+    read(filetext,k);
+    s+=k;
+  end;
+  for i:=1 to (length(s)) do
+  begin
+    if s[i]=' ' then
+      x:=i;     
+  end;
+  delete(s,x,(length(s)-x+1));
+  rewrite(filetext);
+  reset(filetext);
+  for i:=1 to (length(s)) do 
+    begin
+    k:=s[i];
+    write(filetext,k);
+    end;
+  close(filetext);
+end.
