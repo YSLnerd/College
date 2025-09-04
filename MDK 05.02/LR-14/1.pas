65286049046a0dba9d6ea5925e251287071fec6f
YSLnerd <vovalapt23@gmail.com>
2025-09-04 12:30:20 +0300

    Create 1.pas

---
+++ MDK 05.02/LR-14/1.pas
@@ -0,0 +1,13 @@
+﻿procedure row(n:integer);//параметризация
+begin
+     if n >=1 then//декомпозиция 
+       begin
+        write (n, ' ');
+        row(n-2)
+     end;
+     if n=1 then//база
+       write(0);
+end;
+begin
+    row(3);
+end. 
