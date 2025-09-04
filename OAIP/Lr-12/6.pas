5f0e0ea52880ce87333e5ea2938ba422d6a6420f
YSLnerd <vovalapt23@gmail.com>
2025-09-04 13:02:18 +0300

    Create 6.pas

---
+++ OAIP/Lr-12/6.pas
@@ -0,0 +1,23 @@
+﻿program qq;
+var
+  inputfile, textfile: text;
+  line: string;
+begin
+  assign(inputfile, 'D:\основы программирования и алгортмизации\лаба 12\text6.txt');
+  reset(inputFile);
+  assign(textfile, 'D:\основы программирования и алгортмизации\лаба 12\text62.txt');
+  rewrite(textfile);
+  while not eof(inputfile) do
+  begin
+    readln(inputfile, line);
+    if line <> '' then
+    begin
+      writeln(textfile, line);
+    end;
+  end;
+  close(inputfile);
+  close(textfile);
+  erase(inputfile);
+  rename(textfile, 'D:\основы программирования и алгортмизации\лаба 12\text6.txt');
+  writeln('Программа выполнена');
+end.
